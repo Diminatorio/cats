b7224700c8ba8ca67251f5068b1a3eadc9e39708
Diminatorio <104588396+Diminatorio@users.noreply.github.com>
2022-12-24 09:40:47 +0200

    media update

---
+++ src/media/animals.js
@@ -0,0 +1,37 @@
+import cat from './icons/cat.svg'
+import abyssinian from './icons/abyssinian-cat.svg'
+import sokoke from './icons/sokoke-cat.svg'
+import bengal from './icons/bengal-cat.svg'
+import birman from './icons/birman-cat.svg'
+import wirehair from './icons/american-wirehair-cat.svg'
+import bombay from './icons/bombay-cat.svg'
+import serengeti from './icons/serengeti-cat.svg'
+import bobtail from './icons/bobtail-cat.svg'
+import burmese from './icons/burmese-cat.svg'
+import burmilla from './icons/burmilla-cat.svg'
+import shorthair from './icons/british-shorthair-cat.svg'
+import chausie from './icons/chausie-cat.svg'
+import cornish from './icons/cornish-rex-cat.svg'
+import cymric from './icons/cymric-cat.svg'
+import egyptian from './icons/egyptian-mau-cat.svg'
+import highlander from './icons/highlander-cat.svg'
+import himalayan from './icons/himalayan-cat.svg'
+import javanese from './icons/javanese-cat.svg'
+import khao from './icons/khao-manee-cat.svg'
+import minskin from './icons/minskin-cat.svg'
+import korat from './icons/korat-cat.svg'
+import kinkalow from './icons/kinkalow-cat.svg'
+import lambkin from './icons/lambkin-dwarf-cat.svg'
+import laperm from './icons/laperm-cat.svg'
+import maine from './icons/maine-coon-cat.svg'
+import manx from './icons/manx-cat.svg'
+import nebelung from './icons/nebelung-cat.svg'
+import pussy from './icons/pussy-cat.svg'
+import siamese from './icons/siamese-cat.svg'
+
+const animalsArray = [cat, sokoke, bengal, birman, wirehair, bombay,serengeti,
+    bobtail,burmese,burmilla,shorthair,chausie,cornish,cymric,cymric,
+    egyptian,highlander,himalayan,javanese,khao,minskin,korat,kinkalow,
+    lambkin, laperm, maine, manx, nebelung, pussy, siamese, abyssinian]
+
+export default animalsArray
